0b4875aed64b84e5465a478cbb7f23bd5131a990
Scott Robinson <scott.robinson@thoughtworks.com>
2011-10-02 14:32:02 +0800

    Xiao Li: 139

--- try/xli/mars.rb
+++ try/xli/mars.rb
@@ -1,1 +1,1 @@
-F=R=E=1,N=0,L=W=-1,0;S=2;gets;loop{x,y,d=gets.split.map{|c|eval c};gets.chars{|c|c=='M'?(y+=F[d%=4];x+=F[d-1]):d+=eval(c)||0};p [x,y,$0[d]]}
+F=E=1,N=0,W=E-S=2,0;gets;loop{x,y,d=gets.split.map{|c|eval c};gets.bytes{|c|c==77?(y+=F[d%=4];x+=F[d-1]):c>70&&d+=c<=>77};p [x,y,:NESW[d]]}
